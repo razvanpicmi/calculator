329761c090474a17e1ba4f09b9cebba4ad546d3b
razvanpicmi <47541060+razvanpicmi@users.noreply.github.com>
2025-01-06 18:03:22 +0100

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# Solicităm utilizatorului să introducă primul număr
+numar1 = float(input("Introduceți primul număr: "))
+
+# Solicităm utilizatorului să introducă al doilea număr
+numar2 = float(input("Introduceți al doilea număr: "))
+
+# Calculăm suma celor două numere
+suma = numar1 + numar2
+
+# Afișăm rezultatul
+print(f"Suma celor două numere este: {suma}")
